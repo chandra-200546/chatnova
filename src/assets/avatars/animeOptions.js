b03b71a53b3994a84a5b33a85ed1c18efd8f1ddf
chandrashekhar <chandrashekharkumbarias8055@gmail.com>
2026-05-03 16:00:59 +0530

    Expand anime avatar gallery to 1200 options with lazy-loaded previews

--- src/assets/avatars/animeOptions.js
+++ src/assets/avatars/animeOptions.js
@@ -1,3 +1,3 @@
-export const animeAvatarOptions = Array.from({ length: 120 }).map((_, i) =>
+export const animeAvatarOptions = Array.from({ length: 1200 }).map((_, i) =>
   `https://api.dicebear.com/7.x/adventurer/svg?seed=anime-${i + 1}&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf`
 );
